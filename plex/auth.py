25b77dfdce8d463c49735acd4a173a00b726e2e7
Jonrw97 <jonrw97@gmail.com>
2022-04-09 09:40:26 +0200

    login update

--- plex/auth.py
+++ plex/auth.py
@@ -34,7 +34,7 @@ def register():
             else:
                 return redirect(url_for("auth.login"))
         flash(error)
-    return render_template('register.html')
+    return render_template('auth/register.html')
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
@@ -59,7 +59,7 @@ def login():
 
         flash(error)
 
-    return render_template('login.html')
+    return render_template('auth/login.html')
 
 @bp.before_app_request
 def load_logged_in_user():
